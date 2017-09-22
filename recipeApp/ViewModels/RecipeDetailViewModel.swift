0991daeb0132e66696781211b0d9bb35f06e973e
ShahQays Bin Md. Shah <shahqays@innovez-one.com>
2017-09-22 11:36:27 +0800

    Added RecipeDetailViewModel

---
+++ recipeApp/ViewModels/RecipeDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeDetailViewModel.swift
+//  recipeApp
+//
+//  Created by Shah Qays on 22/09/2017.
+//  Copyright © 2017 Shah Qays. All rights reserved.
+//
+
+import Foundation
+
+class RecipeDetailViewModel {
+    
+}
